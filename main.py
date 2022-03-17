72501bf7e574f9472874cd89a076474397cc60bf
otavio <101368339+otaviosz@users.noreply.github.com>
2022-03-17 22:28:43 +0000

    alteração

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ def mundo():
 @app.route('/unifran/')
 def unifran():
   return '<h2>Universidade de Franca</h2>'
-app.run(host='0.0.0.0', port=8080)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0)
